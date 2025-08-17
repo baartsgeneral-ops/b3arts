e1adbe3526e13220bbbdf7477c877336a53afe53
baartsgeneral-ops <baarts.general@gmail.com>
2025-08-17 23:03:08 +0300

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -87,6 +87,7 @@ const About = () => {
               >
                 <div className="w-24 h-24 rounded-full mx-auto mb-6 overflow-hidden">
  <div className="w-24 h-24 rounded-full mx-auto mb-6 overflow-hidden flex items-center justify-center bg-gradient-to-br from-indigo-900 to-indigo-700">
+   
   {member.name === "Sama Hassan" ? (
     <img 
       src="/src/pic/112.jpg"   // ← حطي مكان الصورة هنا
