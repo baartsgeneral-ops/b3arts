e5c87c62162216361d3d90fa69d7faa60fd6d1f7
baartsgeneral-ops <baarts.general@gmail.com>
2025-08-17 07:51:22 +0300

    Updated Podcast.tsx

--- src/pages/Podcast.tsx
+++ src/pages/Podcast.tsx
@@ -8,7 +8,7 @@ const Podcast = () => {
       id: 1,
       title: 'فنون جميلة|مع شهد الشاذلي',
       description: ' في الحلقة دي من zu goes off topic، بنفتح الباب على واحدة من أكتر الكليات اللي حواليها تساؤلات وخرافات: فنون جميلة.',
-      cover: 'https://drive.google.com/file/d/1E78LXLS-IYjfFj23hLydtuo8b0_LnE1H/view?usp=drivesdk',
+      cover: 'https://drive.google.com/uc?export=view&id=1E78LXLS-IYjfFj23hLydtuo8b0_LnE1H',
       duration: '26 min',
       youtubeUrl: 'https://youtube.com/@zugoesofftopic?si=wwXYtEB3zn0u6pUh',
       spotifyUrl: 'https://open.spotify.com/show/4ehspgAgKiDJ6KumEWMTsI?si=QZlI34OjRByFAx4iz0-1fg'
