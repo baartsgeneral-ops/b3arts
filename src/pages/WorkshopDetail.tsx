7cd4355d494e2ca085298110bde1f155d1a984af
baartsgeneral-ops <baarts.general@gmail.com>
2025-08-18 01:40:35 +0300

    Updated WorkshopDetail.tsx

--- src/pages/WorkshopDetail.tsx
+++ src/pages/WorkshopDetail.tsx
@@ -338,7 +338,7 @@ const WorkshopDetail = () => {
         href="https://forms.gle/wRp1EBjwRZnLUeoDA"   
         target="_blank"
         rel="noopener noreferrer"
-              className="bg-gold text-indigo-900 px-8 py-4 rounded-full font-semibold text-lg transition-all duration-300 hover:bg-yellow-400 hover:transform hover:scale-105"
+        className="bg-gold text-indigo-900 px-8 py-4 rounded-full font-semibold text-lg transition-all duration-300 hover:bg-yellow-400 hover:transform hover:scale-105"
             
               Register for the next Workshop
       </a>
