2c04eb54320fa2517ac3cff2113b2aa561f136c0
baartsgeneral-ops <baarts.general@gmail.com>
2025-08-17 22:52:37 +0300

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -47,7 +47,7 @@ const Navigation = () => {
 
           <div className="md:hidden">
             <button className="text-indigo-900 p-2">
-              <Activity size={50} />
+              <Activity size={24} />
             </button>
           </div>
         </div>
