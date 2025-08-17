217e611ede6db5c7703eee8b09da034d3a289e7b
baartsgeneral-ops <baarts.general@gmail.com>
2025-08-18 02:14:50 +0300

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,7 @@ const Navigation = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-indigo-900 rounded-lg flex items-center justify-center">
+            <div className="">
               <img 
   src="/src/pic/logo.png" 
   alt="Logo" 
