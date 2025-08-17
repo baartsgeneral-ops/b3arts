f0ff42c4ee1fb00969d814aee0f991b25e88e287
baartsgeneral-ops <baarts.general@gmail.com>
2025-08-17 23:18:44 +0300

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -49,7 +49,7 @@ const About = () => {
                 >
                   <div className="w-12 h-12 bg-indigo-900 rounded-lg flex items-center justify-center flex-shrink-0">
                     <img 
-                     src="/src/pic/112.jpg"
+                     src="/src/pic/1.jpg"
                       alt={member.name} 
                       className="w-full h-full object-cover"
   />
